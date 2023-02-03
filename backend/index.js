6681ab56ce022b8db68f926e981dded4ba78c6d4
youba idjis <youba.etud@gmail.com>
2023-02-02 18:20:27 -0800

    connecting data base and starting webserver

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import app from "./server";
+import mongodb from 'mongodb';
+import dotenv from 'dotenv';
+
+//to access .env file
+dotenv.config();
+
+const MongoClient = mongodb.MongoClient;
+const port = process.env.PORT;
+
+//connect the database using a URL
+MongoClient.connect(
+    process.env.RESTREVIEWS_DB_URI,
+    {
+        poolSize: 50,
+        wtimeout: 2500,
+        useNewUrlParse: true
+    })
+    .catch(err => {
+        console.error(err.stack);
+        process.exitCode = 1;
+    })
+    .then(async client => {
+        app.listen(port, () => {
+            console.log(`Connected to db & listening on PORT`)
+        })
+    })
+
+
