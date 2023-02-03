12dcb0fe9820b8b0165aeea21269a4c95af9c4fe
youba idjis <youba.etud@gmail.com>
2023-02-02 17:07:24 -0800

    server init

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,12 @@ import cors from "cors";
 import restaurants from "./api/restaurants.route.js";
 
 //creating server
-const app = express()
+const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1/restaurants", restaurants);
+app.use("*", (req, res)=> res.status(404).json({error: "Not Found" }));
+
+export default app;
