8b7f491c78f7f45bd5524082bf316eb22eaf7b19
youba idjis <youba.etud@gmail.com>
2023-02-04 22:03:20 -0800

    setting reviewsCtrl

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,13 +1,15 @@
 import express from "express";
 import RestaurantsController from "./restaurants.controller.js"
+import ReviewsController from "./reviews.controller.js"
+
 const router = express.Router();
 
 router.route("/").get(RestaurantsController.apiGetRestaurants);
 
 router
   .route("/review")
-  .post(ReviewsCtrl.apiPostReview)
-  .put(ReviewsCtrl.apiUpdateReview)
-  .delete(ReviewsCtrl.apiDeleteReview)
-  
+  .post(ReviewsController.apiPostReview)
+  .put(ReviewsController.apiUpdateReview)
+  .delete(ReviewsController.apiDeleteReview)
+
 export default router;
