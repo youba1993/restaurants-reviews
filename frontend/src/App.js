6f9112cf47e594a46752f53d7ecb795db6b17025
youba idjis <youba.etud@gmail.com>
2023-02-13 23:44:39 -0800

    Creating components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import {Switch , Route, Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AddReview from './components/AddReview';
+import Login from './components/Login';
+import Restaurents from './components/Restaurants';
+import RestaurentsList from './components/RestaurentsList';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
+      <AddReview />
+      <Login />
+      <Restaurents />
+      <RestaurentsList />
     </div>
   );
 }
